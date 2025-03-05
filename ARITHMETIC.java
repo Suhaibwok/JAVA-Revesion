3061a1f4ddbbeec0d5e35eadf705aceef382d849
Suhaib <suhaibwok@gmail.com>
2025-03-05 17:40:23 +0530

    Add Arithmatic Operations

--- ARITHMETIC.java
+++ ARITHMETIC.java
@@ -1,3 +1,15 @@
 public class ARITHMETIC {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 10;
+        char C = 'A';
+        float D = 110.123f;
+        boolean E = true;
 
+        System.out.println(A+B);
+        System.out.println(A-D);
+        System.out.println(B+C);
+        System.out.println(A/D);
+        System.out.println(A%D);
+    }
 }
