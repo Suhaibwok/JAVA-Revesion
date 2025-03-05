5f5181e79cfcd66d9245f8fd8927eb73049e838e
Suhaib <suhaibwok@gmail.com>
2025-03-05 14:58:09 +0530

    Add Variables

--- VARIABLES.java
+++ VARIABLES.java
@@ -1,2 +1,18 @@
 public class VARIABLES {
+    public static void main(String[] args) {
+        var A = 100;                int a =100;
+        var B = 'c';                char b ='c';
+        var C = true;               boolean c =false;
+        var D = 10.002;             float d = 10.002f;
+        var E = 10.000333210;       double e = 10.000333210;
+        System.out.println("A = " + A + ", B = " + B + ", C = " + C + ", D = " + D);
+        System.out.println(A+" "+B+" "+C+" "+D);
+        System.out.print(a);   System.out.println(A);
+        System.out.print(b);   System.out.println(B);
+        System.out.print(c);   System.out.println(C);
+        System.out.print(d);   System.out.println(D);
+        System.out.print(e);   System.out.println(E);
+
+    }
 }
+
