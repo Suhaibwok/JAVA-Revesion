98695e1297cda0030d631d81c566b757cbc93936
Suhaib <suhaibwok@gmail.com>
2025-03-06 23:05:50 +0530

    Relational Operations

---
+++ RELATIONAL.java
@@ -0,0 +1,13 @@
+public class RELATIONAL {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 20;
+
+        System.out.println(A<B);
+        System.out.println(A>B);
+        System.out.println(A==B);
+        System.out.println(A!=B);
+        System.out.println(A<=B);
+        System.out.println(A>=B);
+    }
+}
