5c20d2aef92128f67a7600c0a24a3f168dbcc7a0
Suhaib <suhaibwok@gmail.com>
2025-03-06 14:31:31 +0530

    Typecasting

--- TYPECASTING.java
+++ TYPECASTING.java
@@ -1,6 +1,15 @@
 public class TYPECASTING {
     public static void main(String[] args) {
-        char A = (int)'A';
-        System.out.println(A);
+        // IMPLICIT TYPECASTING
+        int x = 10;
+        double y = x;  // Implicit casting from int to double
+        System.out.println(y);  // Output: 10.0
+
+        // EXPLICIT TYPECASTING
+        double a = 9.78;
+        int b = (int) a;  // Explicit casting from double to int
+        System.out.println(b);  // Output: 9 (the fractional part is discarded)
+
+
     }
 }
